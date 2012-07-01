c484e0739ef9a1f667d57d271ff4eaac9d86681c
Nicholas Clark <clarkni5@gmail.com>
2012-07-01 13:01:21 -0400

    Adding file.

---
+++ variable.js
@@ -0,0 +1,17 @@
+﻿
+function is_empty(obj){
+	if (obj instanceof Array) {
+		return h.length === 0;
+	} else if (obj instanceof Object) {
+		for (var i in obj) {
+			return false;
+		}
+		return true;
+	} else {
+		return !obj;
+	}
+}
+
+function is_scalar(obj) {
+	return (/string|number|boolean/).test(typeof obj);
+}
